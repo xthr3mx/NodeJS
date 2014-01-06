d4e9e312e40a126b757e5b72fbdbd89543187c7c
xthr3mx <xthr3mx@gmail.com>
2014-01-06 16:38:04 -0600

    bug fixed::route removed.

--- parameters/app.js
+++ parameters/app.js
@@ -6,7 +6,6 @@
 var express = require('express');
 var routes = require('./routes');
 var estado = require('./routes/estado');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
